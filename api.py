76ff58ead5acb14aea5bcccbba48efb35a1c5cd6
edrianoria <edrianoria982@gmail.com>
2026-02-10 14:10:34 +0800

    Initialize Repository

---
+++ api.py
@@ -0,0 +1,56 @@
+from flask import Flask, request
+from flask_restful import Resource, Api, reqparse
+from flask_mail import Message, Mail
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+app = Flask(__name__)
+api = Api(app)
+
+API_KEY = os.getenv("API_KEY")
+
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USERNAME'] = os.getenv('MAIL_EMAIL')
+app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD')
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
+app.config['MAIL_DEFAULT_SENDER'] = ('Edrian Oria', os.getenv('MAIL_EMAIL'))
+
+mail = Mail(app)
+
+email_args = reqparse.RequestParser()
+email_args.add_argument('email', type=str, required=True, help="Email cannot be blank")
+email_args.add_argument('subject', type=str, required=True, help="Subject cannot be blank")
+email_args.add_argument('message', type=str, required=True, help="Message cannot be blank")
+
+class Email(Resource):
+    def post(self):
+        client_key = request.headers.get("X-API-KEY")
+
+        if client_key != API_KEY:
+            return {"error": "Unauthorized"}, 401
+
+        args = email_args.parse_args()
+        link = os.getenv('PORTFOLIO_LINK')
+        try:
+            message_format = f"""
+            <p>{args["message"]}</p><br><br>
+            <p><strong>Thank you for connecting with me.</strong></p><br>
+            <p>Here is the link to my portfolio website: <br>
+            <a href="{link}">{link}</a></p>
+            """
+
+            message = Message(args["subject"], recipients=[args["email"]])
+            message.html = message_format
+            mail.send(message)
+
+            return {"message": "Email sent successfully"}, 201
+        except:
+            return {"error": "Failed to send email"}, 400
+
+api.add_resource(Email, '/api/email/')
+
+if __name__ == '__main__':
+    app.run(debug=False)
